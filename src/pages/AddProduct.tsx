92e0561ee4d895fd600c0fb79b56e489e125439e
JosipVojkovic <128196492+JosipVojkovic@users.noreply.github.com>
2025-02-27 14:18:05 +0100

    Add styling to AddProduct page

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -6,36 +6,45 @@ export default function AddProduct() {
       <h1>Add Product</h1>
       <form>
         <label htmlFor="title">
-          Title: <input id="title" type="text" placeholder="Enter title" />
+          Title
+          <input id="title" type="text" placeholder="Enter title" />
         </label>
 
-        <label htmlFor="price">
-          Price:{" "}
-          <input
-            id="price"
-            type="number"
-            step="0.01"
-            placeholder="Enter price"
-          />
-        </label>
+        <div>
+          <label htmlFor="price">
+            Price
+            <input
+              id="price"
+              type="number"
+              step="0.01"
+              min={5}
+              placeholder="Enter price"
+            />
+          </label>
+          <label htmlFor="category">
+            Category
+            <select id="category">
+              <option value="men's clothing">Men's clothing</option>
+              <option value="women's clothing">Women's clothing</option>
+              <option value="jewelery">Jewelery</option>
+              <option value="electronics">Electronics</option>
+            </select>
+          </label>
+        </div>
 
         <label htmlFor="description">
-          Description:{" "}
-          <input id="description" type="text" placeholder="Enter description" />
-        </label>
-
-        <label htmlFor="category">
-          Category:{" "}
-          <select id="category">
-            <option value="men's clothing">Men's clothing</option>
-            <option value="women's clothing">Women's clothing</option>
-            <option value="jewelery">Jewelery</option>
-            <option value="electronics">Electronics</option>
-          </select>
+          Description
+          <textarea
+            id="description"
+            placeholder="Enter description"
+            rows={4}
+            cols={30}
+          ></textarea>
         </label>
 
         <label htmlFor="image">
-          Image: <input id="image" type="url" placeholder="Enter URL" />
+          Image
+          <input id="image" type="url" placeholder="Enter URL" />
         </label>
 
         <button type="button">Confirm</button>
